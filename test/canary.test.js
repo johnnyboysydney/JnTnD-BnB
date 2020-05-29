3f9650e450c9b8c779251b263d2e70e84d028a68
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-29 18:23:35 +1000

    Removing all code due to project change

--- test/canary.test.js
+++
@@ -1,9 +0,0 @@
-let expect = require("chai").expect;
-
-describe("canary test", ()=> {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
-  it("should pass this canary test", ()=> {
-    expect(true).to.be.true,
-  });
-});
