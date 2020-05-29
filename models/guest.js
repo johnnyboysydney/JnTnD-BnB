79304ed042963226265a0743aace95e6e2d00aef
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-29 21:30:59 +1000

    Added code to Guest.js

--- models/guest.js
+++ models/guest.js
@@ -1,3 +1,52 @@
 module.exports = function(){
-
+    let Guest = sequelize.define("Guest", {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+          },
+          last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+          },
+          phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          room_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          checkin: {
+            type: DataTypes.DATEONLY
+          },
+          checkout: {
+            type: DataTypes.DATEONLY
+          },
+          isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+          }
+    }, {
+        getterMethods: {
+            isAdmin: function() {
+              return this.getDataValue('isAdmin');
+            }
+        }
+    });
+   
+    Guest.associate = function(models) {
+        Guest.hasOne(models.Room);
+        Guest.hasOne(models.Table);
+      }
+    return Guest;
 };
