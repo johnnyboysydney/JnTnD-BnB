e00d32af2ba376912af0aff8b7963272c994a942
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-30 11:05:21 +1000

    Adding table.js

--- models/table.js
+++ models/table.js
@@ -1,3 +1,44 @@
-module.exports = function(){
-
+module.exports = function() {
+  const Table = sequelize.define("Table", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: ["^[a-z]+$", "i"]
+      }
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        not: ["[a-z]", "i"]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    res_time: {
+      type: DataTypes.TIME,
+      allowNull: false
+    },
+    num_party: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  });
+  Table.associate = function(models) {
+    Table.belongsTo(models.Guest, {
+      onDelete: "cascade"
+    });
+  };
+  return Table;
 };
