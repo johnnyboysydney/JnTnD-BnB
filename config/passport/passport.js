0f7f8c2f5e49faaf48047dc443ba5d2db80acdb3
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-30 11:47:12 +1000

    refactoring passport

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -33,66 +33,161 @@ module.exports = function(passport) {
         });
     }
   });
+
   // Guest Signup
   passport.use(
     "local-signup",
     new LocalStrategy(
       {
-    usernameField: "email",
-    passwordField: "lastname",
-    passReqToCallback: true
-  },
-    (req, email, lastname, done) => {
-        let data = {
-        first_name: req.body.firstname,
-        last_name: lastname,
+        usernameField: "email",
+        passwordField: "lastName",
+        passReqToCallback: true
+      },
+      (req, email, lastName, done) => {
+        const data = {
+          firstName: req.body.firstName,
+          lastName: lastName,
           phone: req.body.phone,
-        email: email,
-          room_number: req.body.room,
+          email: email,
+          roomNumber: req.body.room,
           checkin: req.body.checkin,
-        checkout: req.body.checkout
-      };
+          checkout: req.body.checkout
+        };
 
         Guest.create(data).then((user, created) => {
-      if (!user) {
-        return done(null, false);
-      }
-      else if (user) {
-        db.Room.update({
-          GuestId: user.dataValues.id,
-          available: false
-        }, {
-          where: {
-            id: req.body.room
+          if (!user) {
+            return done(null, false);
+          } else if (user) {
+            db.Room.update(
+              {
+                GuestId: user.dataValues.id,
+                available: false
+              },
+              {
+                where: {
+                  id: req.body.room
+                }
+              }
+            ).then(result => {
+              return done(null, user);
+            });
           }
-        }).then(function(result) {
-          return done(null, user);
         });
       }
-    });
-    })));
-      // Guest signin
-  passport.use('local-signin', new LocalStrategy({
-    usernameField : 'room',
-    passwordField : 'lastname',
-    passReqToCallback : true
-  },
-  function(req, room, lastname, done) {
-    Guest.findOne({
-      where: {
-        room_number: room
+    )
+  );
+
+  // Guest signin
+  passport.use(
+    "local-signin",
+    new LocalStrategy(
+      {
+        usernameField: "room",
+        passwordField: "lastName",
+        passReqToCallback: true
+      },
+      (req, room, lastName, done) => {
+        Guest.findOne({
+          where: {
+            roomNmber: room
+          }
+        })
+          .then(result => {
+            if (!result) {
+              return done(null, false, { message: "User was not found." });
+            }
+            if (result.last_name.toLowerCase() !== lastname.toLowerCase()) {
+              return done(null, false, { message: "Incorrect password." });
+            }
+            return done(null, result);
+          })
+          .catch(err => {
+            console.log("Error:", err);
+            return done(null, false, { message: "Error with sign in" });
+          });
       }
-    }).then(function(result) {
-      if (!result) {
-        return done(null, false, { message: 'User was not found.' });
+    )
+  );
+
+  // Manager signup
+  passport.use(
+    "admin-signup",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      (req, email, password, done) => {
+        const generateHash = function(password) {
+          return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+        };
+        Admin.findOne({
+          where: {
+            email: email
+          }
+        }).then(result => {
+          if (result) {
+            return done(null, false, {
+              message: "Email already exists."
+            });
+          }
+
+          const managerPassword = generateHash(password);
+          const data = {
+            email: email,
+            password: managerPassword,
+            firstName: req.body.firstname,
+            lastName: req.body.lastname
+          };
+          Admin.create(data).then((newManager, created) => {
+            if (!newManager) {
+              return done(null, false);
+            } else if (newManager) {
+              return done(null, newManager);
+            }
+          });
+        });
       }
-      if (result.last_name.toLowerCase() !== lastname.toLowerCase()) {
-        return done(null, false, { message: 'Incorrect password.' });
+    )
+  );
+
+  // Manager signin
+  passport.use(
+    "admin-login",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      (req, email, password, done) => {
+        const isValidPassword = function(userpass, password) {
+          return bCrypt.compareSync(password, userpass);
+        };
+        Admin.findOne({
+          where: {
+            email: email
+          }
+        })
+          .then(result => {
+            if (!result) {
+              return done(null, false, {
+                message: "Email does not exist."
+              });
+            }
+            if (!isValidPassword(result.password, password)) {
+              return done(null, false, { message: "Incorrect password." });
+            }
+            return done(null, result);
+          })
+          .catch(err => {
+            console.log("Error:", err);
+            return done(null, false, {
+              message: "Something went wrong with your Sign-in"
+            });
+          });
       }
-      return done(null, result);
-    }).catch(function(err) {
-      console.log("Error:", err);
-      return done(null, false, { message: 'Error with sign in' });
-    });
-  }));
+    )
+  );
 };
