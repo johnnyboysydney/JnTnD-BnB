fe329640109146ecd99885c6d857ab95e61c4279
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-30 11:12:17 +1000

    adding App express set up and sync models

--- server.js
+++ server.js
@@ -5,4 +5,18 @@ const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
 
-// 
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use("/", (req, res) => {
+    res.render("index");
+  });
+
+// Sync models then start the server to begin listening
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log("----------------------");
+      console.log("App listening on PORT " + PORT);
+    });
+  });
