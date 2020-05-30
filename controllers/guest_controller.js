1e7c8042e17860702794334f49bc09d70154b11c
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-30 11:26:41 +1000

    Refactored code to use arrow functions

--- controllers/guest_controller.js
+++ controllers/guest_controller.js
@@ -1,12 +1,11 @@
 // Dependencies
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.get('/room/book', function(req, res) {
-  db.Room.findAll({
-  }).then(function(result) {
-    res.render("bookroom", {rooms: result});
+router.get("/room/book", (req, res) => {
+  db.Room.findAll({}).then(result => {
+    res.render("bookroom", { rooms: result });
   });
 });
 
