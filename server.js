896c8fea016d7034f0b55c5599ce9a4143c02931
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-30 11:14:44 +1000

    adding passport app use and app dependencies

--- server.js
+++ server.js
@@ -9,9 +9,32 @@ const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Models to sync
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.use(
+    session({
+      secret: "keyboard cat",
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());  
+
 app.use("/", (req, res) => {
     res.render("index");
   });
+
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
+  
+  // Serve files in 'public' directory
+  app.use(express.static("public"));
 
 // Sync models then start the server to begin listening
 db.sequelize.sync().then(() => {
