4065804f8b09e14f0d8294327ec2ac84a13a4171
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-30 11:32:52 +1000

    refactoring code

--- models/guest.js
+++ models/guest.js
@@ -1,52 +1,56 @@
-module.exports = function(){
-    const Guest = sequelize.define("Guest", {
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [1]
-            }
-          },
-          last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [1]
-            }
-          },
-          phone: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false
-          },
-          room_number: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-          },
-          checkin: {
-            type: DataTypes.DATEONLY
-          },
-          checkout: {
-            type: DataTypes.DATEONLY
-          },
-          isAdmin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-          }
-    }, {
-        getterMethods: {
-            isAdmin: function() {
-              return this.getDataValue('isAdmin');
-            }
+module.exports = function() {
+  const Guest = sequelize.define(
+    "Guest",
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
         }
-    });
-   
-    Guest.associate = function(models) {
-        Guest.hasOne(models.Room);
-        Guest.hasOne(models.Table);
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      roomNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      checkin: {
+        type: DataTypes.DATEONLY
+      },
+      checkout: {
+        type: DataTypes.DATEONLY
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
       }
-    return Guest;
+    },
+    {
+      getterMethods: {
+        isAdmin: function() {
+          return this.getDataValue("isAdmin");
+        }
+      }
+    }
+  );
+
+  Guest.associate = function(models) {
+    Guest.hasOne(models.Room);
+    Guest.hasOne(models.Table);
+  };
+  return Guest;
 };
