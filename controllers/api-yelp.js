c66ce93409df6f1df31c5ba4cb4dcccaf52a502b
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-31 16:13:12 +1000

    fixed api-yelp search

---
+++ controllers/api-yelp.js
@@ -0,0 +1,52 @@
+// REQUIRED PACKAGES
+var express = require("express");
+var router = express.Router();
+var yelpKeys = require("../config.js");
+var yelp = require("yelp-fusion");
+
+// YELP FUSION API CALL
+router.get("/yelp", (req, res) => {
+  console.log("YELP API GIT ROUTE SUCCESS");
+  res.render("yelpsearch");
+});
+
+router.post("/api/yelp", (req, res) => {
+  console.log("API POST ROUTE ACTIVE");
+
+  // USER SEARCH TERMS
+  const searchRequest = {
+    term: req.body.term,
+    location: req.body.location
+  };
+
+  //var client = yelp.client(response.jsonBody.access_token);
+  const client = yelp.client(yelpKeys);
+
+  // SEARCH REQUEST
+  client.search(searchRequest).then(
+    response => {
+      const firstResult = response.jsonBody.businesses[0];
+      const prettyJson = JSON.stringify(firstResult, null, 4);
+      console.log(prettyJson);
+      const yelpResponse = response.jsonBody.businesses;
+      var results = [];
+      for (var i = 0; i < 3; i++) {
+        var displayData = {
+          name: yelpResponse[i].name,
+          location: yelpResponse[i].location.display_address,
+          phone: yelpResponse[i].display_phone,
+          open: yelpResponse[i].is_closed,
+          rating: yelpResponse[i].rating
+        };
+        results.push(displayData);
+        console.log("DISPLAY DATA: ", displayData);
+      }
+
+      // Render Yelp Results
+      return res.render("yelpsearch", { business: results });
+    } // close client search .then func
+  );
+});
+
+// EXPORT FUNCTION
+module.exports = router;
